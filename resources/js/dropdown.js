fff7247a3c8d777bc3b9aaca250284bdd306a7a0
BeerMasterTrading <bmtinc.delivery@gmail.com>
2025-05-26 12:32:20 +0800

    Add files via upload

---
+++ resources/js/dropdown.js
@@ -0,0 +1,9 @@
+// Menu Part
+
+    function openMenu() {
+      document.getElementById('sideMenu').classList.add('open');
+    }
+
+    function closeMenu() {
+      document.getElementById('sideMenu').classList.remove('open');
+    }
