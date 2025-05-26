bf2991d40c0ab1120e71389b2333899c218daca5
BeerMasterTrading <bmtinc.delivery@gmail.com>
2025-05-26 12:29:11 +0800

    Delete Resources directory

--- Resources/JS/Dropdown.js
+++
@@ -1,9 +0,0 @@
-// Menu Part
-
-    function openMenu() {
-      document.getElementById('sideMenu').classList.add('open');
-    }
-
-    function closeMenu() {
-      document.getElementById('sideMenu').classList.remove('open');
-    }
